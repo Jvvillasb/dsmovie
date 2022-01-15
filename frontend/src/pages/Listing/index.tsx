58693994e9b3c94d87c4a34c0533a906dd457c41
jvvillasb <jvvillasb@gmail.com>
2022-01-15 13:47:18 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,31 +1,40 @@
-import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination";
+import axios from 'axios';
+import MovieCard from 'components/MovieCard';
+import Pagination from 'components/Pagination';
+import { BASE_URL } from 'utils/requests';
 
-function Listing() {
-    return (
-        <>
-            <Pagination />
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
+import './styles.css';
+
+const Listing = () => {
+	axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+		console.log(response.data);
+	});
+
+	return (
+		<>
+			<Pagination />
+
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Listing;
